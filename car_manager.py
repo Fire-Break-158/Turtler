db7631b893fddccb32631ca7a3560d8344dbebdf
Fire-Break-158 <maxsessionsjr@gmail.com>
2023-09-26 11:33:29 -0500

    Revert "for david to look at class iteration"
    This reverts commit e016bf9da752cc7fc71277089efc024beb5caf6b.

--- car_manager.py
+++ car_manager.py
@@ -1,40 +1,43 @@
 from turtle import Turtle, Screen
 import random
 
-SPAWN_Y = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+SPAWN_Y = [-400, -300, -200, -100, 0, 100, 200, 300, 400]
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 50
-SPAWN_X = [500, -500]
+SPAWN_X = [475, -475]
 MOVE_INCREMENT = 10
 
+#screen = Screen()
+#screen.bgcolor("black")
+#screen.setup(width=1000, height=1000)
+#screen.title("Car test")
 
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(width=1000, height=1000)
-screen.title("Car test")
+class CarManager(Turtle):
+    def __init__(self, screen, name):    
+        super().__init__()
+        print('hello')
+        self = Turtle()
+        self.hideturtle()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_wid=3, stretch_len=1.5)
+        self.penup()
+        self.setheading(90)
+        self.goto(random.choice(SPAWN_X), random.choice(SPAWN_Y))
+        self.showturtle()
 
-#class CarManager:
-    
-car = Turtle()
-car.hideturtle()
-car.shape("square")
-car.color(random.choice(COLORS))
-car.shapesize(stretch_wid=3, stretch_len=1.5)
-car.penup()
-car.setheading(90)
-car.goto(random.choice(SPAWN_X), random.choice(SPAWN_Y))
-car.showturtle()
+        def move(self):
+            if self.xcor() == -475:
+                while self.xcor != 475:
+                    newx = self.xcor() + MOVE_INCREMENT
+                    self.goto(newx, self.ycor())
+            elif self.xcor() == 475:
+                while self.xcor != -475:
+                    newx = self.xcor() - MOVE_INCREMENT
+                    self.goto(newx, self.ycor())
 
-def move():
-    if car.xcor() == -500:
-        while car.xcor != 500:
-            newx = car.xcor() + MOVE_INCREMENT
-            car.goto(newx, car.ycor())
-    elif car.xcor() == 500:
-        while car.xcor != -500:
-            newx = car.xcor() - MOVE_INCREMENT
-            car.goto(newx, car.ycor())
+        move(self)
 
-move()
+        pass
 
-screen.exitonclick()
+#screen.exitonclick()
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import time
 from turtle import Screen
-from player import Player
+#from player import Player, STARTING_POSITION
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
+screen.bgcolor("black")
+screen.setup(width=1000, height=1000)
+screen.title("Main test")
+
+#player = Player(STARTING_POSITION, screen)
+
+car = CarManager(screen, '1')
+
+car1 = CarManager(screen, '2')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(1)
+    time.sleep(0.1)
     screen.update()
